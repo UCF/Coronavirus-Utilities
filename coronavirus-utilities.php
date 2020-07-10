d022bc0bb41751bc77cd867db46f16be0ebcfe3f
Jo Dickson <jogreybill@gmail.com>
2020-07-10 12:18:00 -0400

    Added initial options page for configuring weekly emails

--- coronavirus-utilities.php
+++ coronavirus-utilities.php
@@ -8,6 +8,14 @@ License: GPL3
 GitHub Plugin URI: UCF/Coronavirus-Utilities
 */
 
+namespace Coronavirus\Utils;
+
+
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
+
+define( 'CORONAVIRUS_UTILS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+
+require_once CORONAVIRUS_UTILS__PLUGIN_DIR . 'includes/options-weekly-email.php';
