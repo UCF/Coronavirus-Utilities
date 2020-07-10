bbfe86e97b13e7634e7e52849e3595e33d38e29f
Jo Dickson <jogreybill@gmail.com>
2020-07-10 10:00:39 -0400

    Initial commit

---
+++ coronavirus-utilities.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Coronavirus Utilities
+Description:  Utility and feature plugin for the UCF Coronavirus website.
+Version: 0.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/Coronavirus-Utilities
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
