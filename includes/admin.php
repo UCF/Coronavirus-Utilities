b2fcbee211c12077d94e30d5c58b054da91bf735
Jo Dickson <jogreybill@gmail.com>
2020-07-17 11:52:46 -0400

    Added element whitelists for email paragraph fields and article deck fields

--- includes/admin.php
+++ includes/admin.php
@@ -25,3 +25,37 @@ function acf_toolbars( $toolbars ) {
 }
 
 add_filter( 'acf/fields/wysiwyg/toolbars', __NAMESPACE__ . '\acf_toolbars' );
+
+
+/**
+ * Update TinyMCE settings for ACF WYSIWYG fields.
+ *
+ * @see https://www.tiny.cloud/docs-3x/reference/configuration/Configuration3x@valid_elements/
+ * @since 1.0.0
+ * @author Jo Dickson
+ * @return void
+ */
+function acf_configure_tinymce() {
+?>
+<script type="text/javascript">
+acf.add_filter('wysiwyg_tinymce_settings', function(mceInit, id, $field){
+
+	var fieldID = $field.data('key');
+
+	// Paragraph field
+	if (fieldID === 'field_5ec43f369be15') {
+		mceInit.valid_elements = 'a[href],strong/b,em/i,p,ul,ol,li,sup,sub';
+	}
+	// Article deck field
+	else if (fieldID === 'field_5ec443edb99ec') {
+		mceInit.valid_elements = 'a[href],strong/b,em,sup,sub';
+	}
+
+	return mceInit;
+
+});
+</script>
+<?php
+}
+
+add_action( 'acf/input/admin_footer', __NAMESPACE__ . '\acf_configure_tinymce' );
